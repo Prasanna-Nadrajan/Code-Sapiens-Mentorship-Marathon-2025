8d4b173d0b06c31c36e28bba1ddb05f230684ebd
Prasanna Nadrajan <prasannanadrajan@gmail.com>
2025-11-18 12:18:52 +0530

    adjusted vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// Stream_Verse/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 💡 Ensure no 'base' property is here!
 })
